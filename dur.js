6776bd98eed53b410809bb323a265c4572272f76
Sebastian Vollnhals <sebastian@vollnhals.info>
2014-05-09 19:17:54 +0200

    <3

---
+++ dur.js
@@ -0,0 +1,43 @@
+#!/sur/bin/env node
+
+var dur = module.exports = function(s) {
+	var tot = 0.0;
+	s.toLowerCase().match(/[0-9]+([\.,][0-9]+)?\s*(weeks|week|day|days|min|mins|minutes|minute|hrs|hr|hour|hours|sec|secs|second|seconds|[wdhms])/g).forEach(function(m){
+		var match = m.match(/^(([0-9]+)([\.,][0-9]+)?)\s*(weeks|week|day|days|min|mins|minutes|minute|hrs|hr|hour|hours|sec|secs|second|seconds|[wdhms])$/);
+		var num = parseFloat(match[1].replace(/,/g,'.'));
+		switch(match[4]) {
+			case "s":
+			case "sec":
+			case "secs":
+			case "second":
+			case "seconds":
+				tot += (num * 1000);
+			break;
+			case "m":
+			case "min":
+			case "mins":
+			case "minute":
+			case "minutes":
+				tot += (num * 60000);
+			break;
+			case "h":
+			case "hr":
+			case "hrs":
+			case "hour":
+			case "hours":
+				tot += (num * 3600000);
+			break;
+			case "d":
+			case "day":
+			case "days":
+				tot += (num * 86400000);
+			break;
+			case "w":
+			case "week":
+			case "weeks":
+				tot += (num * 604800000);
+			break;
+		}
+	});
+	return Math.round(tot);
+};
